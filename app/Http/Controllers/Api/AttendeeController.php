16307e7e1bd6d5316973c6d152222d4a582bc0bf
bitf12m033 <umermj2@gmail.com>
2023-10-17 23:04:52 +0500

    86 Throttle the API
    Rate Limiting
    Throttling using the middleware inside the controller
    RouteServiceProvider.php

--- app/Http/Controllers/Api/AttendeeController.php
+++ app/Http/Controllers/Api/AttendeeController.php
@@ -17,6 +17,7 @@ class AttendeeController extends Controller
 
     public function __construct(){
         $this->middleware('auth:sanctum')->except(['index','show' ,'update']);
+        $this->middleware('throttle:api')->only(['store' ,'destroy']);
         $this->authorizeResource(Attendee::class , 'attendee');
     }
     /**
@@ -37,7 +38,7 @@ class AttendeeController extends Controller
     {
         $attendee = $this->loadRelationships(
             $event->attendees()->create([
-                'user_id' => 1,
+                'user_id' => $request->user()->id,
             ])
         );
 
--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -18,6 +18,8 @@ class EventController extends Controller
 
     public function __construct(){
         $this->middleware('auth:sanctum')->except(['index','show']);
+        // $this->middleware('throttle:60,1')->only(['store','destroy' ,'update']);
+        $this->middleware('throttle:api')->only(['store','destroy' ,'update']);
         $this->authorizeResource(Event::class , 'event');
     }
     /**
