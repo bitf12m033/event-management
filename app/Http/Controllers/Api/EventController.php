2e6227d70f36d1c1abeba9bdc77c515e9f4a495c
bitf12m033 <umermj2@gmail.com>
2023-10-17 15:26:14 +0500

    72 - Optional Relation Loading
    adde include query param to load specific data.
    control the relation loading with $relations array.

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -16,9 +16,33 @@ class EventController extends Controller
     public function index()
     {
         // return Event::all();
-        return EventResource::collection(Event::with('user')->paginate());
+        $query = Event::query();
+
+        $relations = ['user' , 'attendees' ,'attendees.user'];
+        
+        foreach($relations as $relation) {
+
+            $query->when(
+                $this->shouldIncludeRelation($relation),
+                fn($q) => $q->with($relation)
+            );
+        }
+
+        return EventResource::collection($query->latest()->paginate());
+        // $this->shouldIncludeRelation('user');
+        // return EventResource::collection(Event::with('user')->paginate());
     }
 
+    protected function shouldIncludeRelation(string $relation) : bool {
+        $include = request()->query('include');
+        if(!$include) {
+            return false;
+        }
+
+        $relations = array_map('trim',explode(',',$include));
+        
+        return in_array($relation , $relations);
+    }
     /**
      * Store a newly created resource in storage.
      */
