ad8c9883c0abd938b7b0146d40265b6d180afe9e
bitf12m033 <umermj2@gmail.com>
2023-10-17 22:50:54 +0500

    85 - Schedule Queues
    php artisan queue:table
    php artisan queue:work
    Monitoring tool Supervisor

--- app/Notifications/EventReminderNotification.php
+++ app/Notifications/EventReminderNotification.php
@@ -8,7 +8,7 @@ use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class EventReminderNotification extends Notification
+class EventReminderNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
