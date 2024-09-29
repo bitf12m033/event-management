56c138c02a62de899969fba9a708c3080177a86f
Abaid Ur Rehman <arehman@vinsuite.com>
2024-09-29 16:04:58 +0500

    correct validations

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -14,7 +14,7 @@ class AuthController extends Controller
 {
     try {
         $request->validate([
-            'phone' => 'required',
+            'phone' => 'required|exists:users,phone',
             'pin' => 'required'
         ]);
     } catch (ValidationException $e) {
@@ -29,7 +29,8 @@ class AuthController extends Controller
 
     if (!$user || !Hash::check($request->pin, $user->password)) {
         return response()->json([
-            'message' => 'The provided credentials are incorrect.',
+            'message' => 'Login failed.',
+            'errors' => ["pin" => ["The provided pin is incorrect."]],
             'success' => false
         ]);
     }
@@ -42,46 +43,6 @@ class AuthController extends Controller
         'user' => $user
     ]);
 }
-
-public function signup(Request $request)
-    {
-
-        try {
-            
-            $request->validate([
-                'name' => 'required|string|max:255',
-                'email' => 'required|string|email|max:255|unique:users',
-                'phone' => 'required|string|unique:users',
-                'pin' => 'required|string|min:4|max:6',
-            ]);
-
-
-        } catch (ValidationException $e) {
-         
-            return response()->json([
-                'success' => false,
-                'message' => 'Validation failed',
-                'errors' => $e->errors()
-            ]);
-        }
-
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'phone' => $request->phone,
-            'password' => Hash::make($request->pin),
-            'is_phone_verified' => false, // Set to false initially
-        ]);
-
-        $token = $user->createToken('api-token')->plainTextToken;
-
-        return response()->json([
-            'message' => 'User registered successfully',
-            'user' => $user,
-            'token' => $token,
-            'success' => true,
-        ], 201);
-    }
 
     public function logout(Request $request){
         $request->user()->tokens()->delete();
